8047bcf9aee08ccd9aa070c3daef3bbebe164d06
Gerrard English <gerrardenglish@hotmail.com>
2025-01-28 11:34:33 +0000

    fix get spectra pen scripts to sort rep numbers

--- Scripts/get_tidy_specpen.R
+++ Scripts/get_tidy_specpen.R
@@ -14,7 +14,7 @@ df1 <- data.frame(ID = str_sub(strsplit(spec.csv[1], split = '/')[[1]][6], 0,-5)
                   time = sapply(1:15, function(x){strsplit(as.character(unname(f1[1,-c(1,17)])), split = ' ')[[x]][2]}),
                   nm = as.numeric(f1$Transmitance[-c(1:4)]),
                   refl = as.numeric(bind_rows(lapply(2:16, function(x){data.frame(r = f1[,x][-c(1:4)])}))$r),
-                  rep = rep(2:16, each=256)
+                  rep = rep(1:15, each=256)
 )
 # the remaing files are tidied and combined with the first 
 df2 <- bind_rows(lapply(spec.csv[2:8], tidy_specpen_data))
@@ -27,6 +27,8 @@ df <- bind_rows(lapply(spec.csv[2:8], tidy_specpen_data), df1)
 
 
 write.csv(df, './Data/tidy_spectrapen.csv')
+
+path <-spec.csv[2]
 
 ggplot(df, aes( x = nm, y = refl, colour = as.factor(rep)))+
   geom_line() +
